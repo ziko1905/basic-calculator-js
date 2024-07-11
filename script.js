0f06e13db7ed2710fc74ad9d48ac01177b5943eb
Karlo Cehulic <karlocehlic@gmail.com>
2024-07-12 00:50:44 +0200

    add operational buttons

--- script.js
+++ script.js
@@ -16,44 +16,51 @@ function createCalcBtns() {
     }
 
     function createNumberBtns() {
-        const numbersDiv = document.querySelector(".number-buttons")
+        const numbersDiv = document.querySelector(".number-buttons");
         let rows = [];
         for (let n = 0; n < 4; n++) {
-            const currRow = document.createElement("div")
-            currRow.setAttribute("class", "rows gap")
-            numbersDiv.appendChild(currRow)
-            rows.unshift(currRow)
+            const currRow = document.createElement("div");
+            currRow.setAttribute("class", "rows gap");
+            numbersDiv.appendChild(currRow);
+            rows.unshift(currRow);
         }
         for (let m = 1; m < 10; m++) {
-            let i = Math.floor((m-1) / 3) + 1
-            let row = rows[i]
-            const btn = document.createElement("button")
-            btn.textContent = m
-            console.log(m)
+            let i = Math.floor((m-1) / 3) + 1;
+            let row = rows[i];
+            const btn = document.createElement("button");
+            btn.textContent = m;
+            console.log(m);
 
-            row.appendChild(btn)
+            row.appendChild(btn);
         }
         const lastRow = rows[0];
         const LAST_ROW_VALS = ["0", ".", "Ans"];
         for (let p = 0; p < 3; p++) {
-            const btn = document.createElement("button")
-            btn.textContent = LAST_ROW_VALS[p]
+            const btn = document.createElement("button");
+            btn.textContent = LAST_ROW_VALS[p];
 
-            lastRow.appendChild(btn)
+            lastRow.appendChild(btn);
         }
 
-        console.log(rows)
     }
 
     function createOperBtns () {
-        const operDiv = document.querySelector(".operation-buttons")
+        const operDiv = document.querySelector(".operation-buttons");
+        const OPER_ROW_VALS = ["=", "Del", "X", "/", "+", "-", "(", ")"];
         let rows = [];
 
         for (let n = 0; n < 4; n++) {
-            const row = document.createElement("div")
-            row.setAttribute("class", "rows gap")
-            rows.push(row)
-            operDiv.appendChild(row)
+            const row = document.createElement("div");
+            row.setAttribute("class", "rows gap");
+            rows.push(row);
+            operDiv.appendChild(row);
+        }
+        for (let m = 0; m < 8; m++) {
+            const row = rows[Math.floor(m/2)];
+            const btn = document.createElement("button");
+
+            btn.textContent = OPER_ROW_VALS[m]
+            row.appendChild(btn)
         }
     }
 
