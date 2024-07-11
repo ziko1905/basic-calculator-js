4e16e232e2a460895da58d42fc3e0094fb2158af
Karlo Cehulic <karlocehlic@gmail.com>
2024-07-11 23:36:01 +0200

    add gap for spacing of buttons and fix placement of functional buttons

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ function createCalcBtns() {
         const onOffDiv = document.querySelector("#on-of-button");
         const arrowLeftBtn = document.createElement("button");
         const arrowRightBtn = document.createElement("button");
+        const onOffBtn = document.createElement("button")
 
-        arrowLeftBtn.textContent = "<"
-        arrowRightBtn.textContent = ">"
+        arrowLeftBtn.textContent = "<";
+        arrowRightBtn.textContent = ">";
+        onOffBtn.textContent = "On/Off";
 
         arrowsDiv.appendChild(arrowLeftBtn);
         arrowsDiv.appendChild(arrowRightBtn);
+        onOffDiv.appendChild(onOffBtn);
     }
 
     createFunctBtns()
