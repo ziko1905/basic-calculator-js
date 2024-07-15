eae3f417146cfc1c8fed806b95e25f5249a8406c
Karlo Cehulic <karlocehlic@gmail.com>
2024-07-15 16:53:24 +0200

    clear unnecesary code and make things more symetrical, add semicolons

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let calcArr = [];
 let prevAns = 0;
+let currCharPos = -1;
+
 const calcScreen = document.querySelector("canvas");
 const calcContext = calcScreen.getContext("2d")
 const SCREEN_WIDTH = getComputedStyle(calcScreen).width;
@@ -7,15 +9,15 @@ const SCREEN_HEIGHT = getComputedStyle(calcScreen).height;
 const SCREEN_WIDTH_NUMBER = +SCREEN_WIDTH.split("").slice(0, -2).join("")
 const SCREEN_HEIGHT_NUMBER = +SCREEN_HEIGHT.split("").slice(0, -2).join("")
 const STARTING_X_POS = 9;
-
-let currCharPos = -1;
-let drawX = STARTING_X_POS;
-const DRAW_EQU_Y = 50
+const ELEMENTS_GAP = 28;
+const DRAW_EQU_Y = 50;
 const TARGET_LINE_INTERVAL = 0.5; //interval in seconds
+
+let drawX = STARTING_X_POS;
 
 
 calcScreen.setAttribute("width", SCREEN_WIDTH);
-calcScreen.setAttribute("height", SCREEN_HEIGHT)
+calcScreen.setAttribute("height", SCREEN_HEIGHT);
 
 function createCalcBtns() {
     function createFunctBtns() {
@@ -74,12 +76,14 @@ function createCalcBtns() {
             rows.push(row);
             operDiv.appendChild(row);
         }
+
         for (let m = 0; m < 8; m++) {
             const row = rows[Math.floor(m/2)];
             const btn = Button();
 
             if (OPER_ROW_VALS[m] == "Del") {
                 btn.setAttribute("id", "special-buttons");
+        
             }
             btn.textContent = OPER_ROW_VALS[m];
             row.appendChild(btn);
@@ -87,27 +91,27 @@ function createCalcBtns() {
     }
 
     
-    createNumberBtns()
-    createFunctBtns()
-    createOperBtns()
+    createNumberBtns();
+    createFunctBtns();
+    createOperBtns();
+
 }
 
 createCalcBtns()
 
 function clearCalcScreen() {
-    calcContext.clearRect(0, 0, calcScreen.width, calcScreen.height)
-    calcArr = []
+    calcContext.clearRect(0, 0, calcScreen.width, calcScreen.height);
+    calcArr = [];
     drawX = STARTING_X_POS;
-    console.log("screen cleared")
 
 }
 
 function deleteCurrChar() {
     if (currCharPos == -1) return
-    calcArr.splice(currCharPos, 1)
-    clearTargetLine()
-    drawX -= 28;
-    draw(currCharPos)
+    calcArr.splice(currCharPos, 1);
+    clearTargetLine();
+    drawX -= ELEMENTS_GAP;
+    draw(currCharPos);
     currCharPos--
     
 }
@@ -115,63 +119,58 @@ function deleteCurrChar() {
 function moveLeft() {
     if (currCharPos == -1) return
     currCharPos--
-    clearTargetLine()
-    drawX -= 28
+    clearTargetLine();
+    drawX -= ELEMENTS_GAP;
 }
 
 function moveRight() {
     if (currCharPos == calcArr.length - 1) return
     currCharPos++
-    clearTargetLine()
-    drawX += 28
+    clearTargetLine();
+    drawX += ELEMENTS_GAP;
 }
 
-function Button(func) {
+function Button() {
     let obj = document.createElement("button");
     obj.addEventListener("mousedown", (e) => {
         this.lightColor = getComputedStyle(e.target).backgroundColor;
-        let dimColor = getRgbFromString(this.lightColor)
+        let dimColor = getRgbFromString(this.lightColor);
         const DIM_PERCENTAGE = 0.75;
 
-        e.target.style.backgroundColor = `rgb(${dimColor[0] * DIM_PERCENTAGE}, ${dimColor[1] * DIM_PERCENTAGE}, ${dimColor[2] * DIM_PERCENTAGE})`
+        e.target.style.backgroundColor = `rgb(${dimColor[0] * DIM_PERCENTAGE}, ${dimColor[1] * DIM_PERCENTAGE}, ${dimColor[2] * DIM_PERCENTAGE})`;
     })
 
     obj.addEventListener("click", (e) => {
         e.target.style.backgroundColor = this.lightColor;
         let pressedValue = e.target.textContent;
 
-        if (pressedValue == "Clear") clearCalcScreen()
-        else if (pressedValue == "Del") deleteCurrChar()
-        else if (pressedValue == "<") moveLeft()
-        else if (pressedValue == ">") moveRight()
+        if (pressedValue == "Clear") clearCalcScreen();
+        else if (pressedValue == "Del") deleteCurrChar();
+        else if (pressedValue == "<") moveLeft();
+        else if (pressedValue == ">") moveRight();
         else {
-            calcArr.splice(currCharPos + 1, 0, pressedValue)
+            calcArr.splice(currCharPos + 1, 0, pressedValue);
             currCharPos++
-            draw(currCharPos)
-            drawX += 28;
+            draw(currCharPos);
+            drawX += ELEMENTS_GAP;
         }
 
     })
 
     function getRgbFromString(string) {
-        return string.split("").slice(4, -1).join("").split(", ")
+        return string.split("").slice(4, -1).join("").split(", ");
     }
 
     return obj
 }
 
-function addToExpression(string) {
-    calcArr.push(string)
-}
-console.log(SCREEN_WIDTH_NUMBER)
-
 function draw(index) {
     calcContext.font = "48px serif"
-    calcContext.clearRect(drawX, DRAW_EQU_Y + 10, SCREEN_WIDTH_NUMBER, -52)
-    let tmpX = drawX
+    calcContext.clearRect(drawX, DRAW_EQU_Y + 10, SCREEN_WIDTH_NUMBER, -52);
+    let tmpX = drawX;
     for (let n = index; n < calcArr.length; n++) {
         calcContext.fillText(calcArr[n], tmpX, DRAW_EQU_Y, 24);
-        tmpX += 28;
+        tmpX += ELEMENTS_GAP;
     }
     
     clearTargetLine()
@@ -181,10 +180,9 @@ function draw(index) {
 function drawTargetLine() {
     calcContext.fillRect(drawX - 4, DRAW_EQU_Y, 4, -32);
     window.setTimeout(() => {
-        clearTargetLine()
-        window.setTimeout(() => {drawTargetLine()}, TARGET_LINE_INTERVAL * 1000)
+        clearTargetLine();
+        window.setTimeout(() => {drawTargetLine()}, TARGET_LINE_INTERVAL * 1000);
     }, TARGET_LINE_INTERVAL * 1000)
-    
     
 }
 
